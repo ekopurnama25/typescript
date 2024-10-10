ddc200316add07dabbfaa1526bfbd03e8f5071a9
unknown <ekopurnama250498@gmail.com>
2024-10-10 09:51:40 +0700

    first commit

--- src/pages/Login/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div>
-      <h1>Ini Halaman </h1>
-    </div>
-  );
-}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,10 @@
 import { RouteObject } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/home",
     element: <Home />,
   },
   {
